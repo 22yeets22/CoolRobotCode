feac03f78cc778f43ef228a998cdcd43c4a7576c
22yeets22 <132288501+22yeets22@users.noreply.github.com>
2024-09-14 14:02:22 -0700

    Create comp-v1.1.cpp

---
+++ robot-1/robot-v1/comp-v1.1.cpp
@@ -0,0 +1,218 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       lekan                                                     */
+/*    Created:      9/14/2024, 2:10:26 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain Brain;
+
+// A global instance of competition
+competition Competition;
+
+// Define all needed devices:
+controller Controller = controller(primary);
+motor leftMotorA = motor(PORT10, ratio18_1, true);
+motor leftMotorB = motor(PORT11, ratio18_1, true);
+motor_group leftDriveSmart = motor_group(leftMotorA, leftMotorB);
+motor rightMotorA = motor(PORT1, ratio18_1, false);
+motor rightMotorB = motor(PORT20, ratio18_1, false);
+motor_group rightDriveSmart = motor_group(rightMotorA, rightMotorB);
+drivetrain Drivetrain = drivetrain(leftDriveSmart, rightDriveSmart, 319.19, 295, 40, mm, 1);
+motor conveyor = motor(PORT14, ratio18_1, true);
+motor intake = motor(PORT2, ratio18_1, false);
+digital_out piston = digital_out(Brain.ThreeWirePort.A);
+
+// Define variable for remote controller enable/disable
+bool remoteControlCodeEnabled = true;
+bool stuckHelperCodeEnabled = true;
+// Define variables used for controlling motors based on controller inputs
+bool controllerLeftShoulderControlMotorsStopped = true;
+bool controllerRightShoulderControlMotorsStopped = true;
+bool drivetrainLNeedsToBeStoppedController = true;
+bool drivetrainRNeedsToBeStoppedController = true;
+// Piston
+bool pistonState = false;
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*---------------------------------------------------------------------------*/
+
+void initialize() {
+    // All activities that occur before the competition starts.
+    Drivetrain.setDriveVelocity(100, percent);
+    Drivetrain.setTurnVelocity(100, percent);
+    Drivetrain.setStopping(coast);
+    conveyor.setVelocity(100, percent);
+    conveyor.setMaxTorque(100, percent);
+    conveyor.setStopping(coast);
+    intake.setVelocity(100, percent);
+    intake.setMaxTorque(100, percent);
+    intake.setStopping(coast);
+
+    Brain.Screen.setFont(mono30);
+}
+
+/*---------------------------------------------------------------------------*/
+/*                              Autonomous Task                              */
+/*---------------------------------------------------------------------------*/
+
+void autonomous() {
+    // All the autonomous code
+}
+
+/*---------------------------------------------------------------------------*/
+/*                              User Control Task                            */
+/*---------------------------------------------------------------------------*/
+
+void controlDrivetrain() {
+    // Function to handle drivetrain motor control
+    int drivetrainLeftSideSpeed = Controller.Axis3.position();
+    int drivetrainRightSideSpeed = Controller.Axis2.position();
+
+    auto handleMotorControl = [](motor_group & motorGroup, int speed, bool & motorNeedsToBeStopped) {
+        if (abs(speed) < 5) {
+            if (motorNeedsToBeStopped) {
+                motorGroup.stop();
+                motorNeedsToBeStopped = false;
+            }
+        } else {
+            motorNeedsToBeStopped = true;
+            motorGroup.setVelocity(speed, percent);
+            motorGroup.spin(forward);
+        }
+    };
+
+    handleMotorControl(leftDriveSmart, drivetrainLeftSideSpeed, drivetrainLNeedsToBeStoppedController);
+    handleMotorControl(rightDriveSmart, drivetrainRightSideSpeed, drivetrainRNeedsToBeStoppedController);
+}
+
+void controlPiston() {
+    static bool lastButtonState = false; // Track the previous state of the button
+
+    bool currentButtonState = Controller.ButtonA.pressing();
+
+    if (currentButtonState && !lastButtonState) { // Detect button press
+        pistonState = !pistonState; // Toggle the piston state
+        piston.set(pistonState);
+    }
+
+    lastButtonState = currentButtonState; // Update the previous state
+}
+
+void controlConveyor() {
+    // Function to handle conveyor control
+    if (Controller.ButtonL1.pressing()) {
+        conveyor.spin(forward);
+        controllerLeftShoulderControlMotorsStopped = false;
+    } else if (Controller.ButtonL2.pressing()) {
+        conveyor.spin(reverse);
+        controllerLeftShoulderControlMotorsStopped = false;
+    } else if (!controllerLeftShoulderControlMotorsStopped) {
+        conveyor.stop();
+        controllerLeftShoulderControlMotorsStopped = true;
+    }
+}
+
+void controlIntake() {
+    // Function to handle intake control
+    if (Controller.ButtonR1.pressing()) {
+        intake.spin(forward);
+        controllerRightShoulderControlMotorsStopped = false;
+    } else if (Controller.ButtonR2.pressing()) {
+        intake.spin(reverse);
+        controllerRightShoulderControlMotorsStopped = false;
+    } else if (!controllerRightShoulderControlMotorsStopped) {
+        intake.stop();
+        controllerRightShoulderControlMotorsStopped = true;
+    }
+}
+
+int stuckHelperLoop() {
+    if (!stuckHelperCodeEnabled) {
+        return 0;
+    }
+
+    while (true) {
+        // if too high current and too low velocity, stop
+        wait(20, msec);
+    }
+    return 0;
+}
+
+int controllerLoop() {
+    // Handle controller input
+    while (true) {
+        if (remoteControlCodeEnabled) {
+            controlDrivetrain();
+            controlConveyor();
+            controlIntake();
+            controlPiston();
+        }
+        wait(20, msec);
+    }
+}
+
+int infoLoop() {
+    // Prints info on brain & controller
+    while (true) {
+        Brain.Screen.clearScreen();
+        Brain.Screen.setCursor(1, 1);
+
+        double infoToPrint[] = {
+            static_cast<double>(Brain.Battery.capacity()),
+            static_cast<double>(Brain.Battery.current()),
+            static_cast<double>(Brain.Battery.temperature()),
+            static_cast<double>(Drivetrain.efficiency()),
+            static_cast<double>(Drivetrain.torque())
+        };
+
+        // Iterate over the array and print each value
+        for (double info : infoToPrint) {
+            Brain.Screen.print(info);
+            Brain.Screen.newLine();
+        }
+
+        wait(200, msec); // Give some delay
+    }
+    return 0;
+}
+
+void userControl() {
+    Controller.rumble(". . . -");
+
+    task controllerTask(controllerLoop);
+    task stuckHelperTask(stuckHelperLoop);
+    task infoTask(infoLoop);
+}
+
+// Set up callbacks and stuff
+int main() {
+    // Set up callbacks for autonomous and driver control periods.
+    Competition.autonomous(autonomous);
+    Competition.drivercontrol(userControl);
+
+    // Run the pre-autonomous function.
+    initialize();
+
+    // Prevent main from exiting with an infinite loop.
+    while (true) {
+        wait(1000, msec);
+    }
+
+    return 0;  // Prevents compiler warnings
+}
